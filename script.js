a0bf0c7cc7959f26f1d1558b7a7402b6dfe05023
leonnit NOMENJANAHARY <leonnit@localhost.localdomain>
2025-07-07 20:41:18 +0300

    mise a jour des fichier et CV

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ const nav = document.getElementById('nav');
 menu.addEventListener('click', ()=>{
     nav.classList.toggle('active');
 });
+
+const button = document.getElementById("button");
+
+button.addEventListener('click', () =>{
+    const link = document.createElement("a");
+    link.href = "cv/mon CV.pdf";
+    link.download = "CV_Leonnit.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
+
+const cv = document.getElementById("cv");
+
+cv.addEventListener('click', () =>{
+    const link = document.createElement("a");
+    link.href = "cv/mon CV.pdf";
+    link.download = "CV_Leonnit.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
