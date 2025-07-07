c6289cffb283ebb5742f36c36fc92fce81f17a7b
leonnit NOMENJANAHARY <leonnit@localhost.localdomain>
2025-07-07 21:40:34 +0300

    mije a jour email

--- script.js
+++ script.js
@@ -25,3 +25,44 @@ cv.addEventListener('click', () =>{
     link.click();
     document.body.removeChild(link);
 });
+
+form = document.getElementById('contactForm');
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const nom = document.getElementById('nom').value;
+    const message = document.getElementById('message').value;
+
+    const serviceID = 'service_jxh6qnd';
+    const templateID = 'template_1wlmtz6';
+    const userID = 'tGL9Mzz0qL_k0VQaG';
+
+    const templateParams = {
+        from_name: nom,
+        from_email: email,
+        message: message
+    };
+
+    try {
+        await emailjs.send(serviceID, templateID, templateParams, userID);
+        alert('Message envoyé avec succès !');
+        document.getElementById('contactForm').reset();
+    } catch (error) {
+        alert('Erreur lors de l\'envoi du message. Veuillez réessayer.');
+    } 
+});
+
+(function(){
+    emailjs.init('tGL9Mzz0qL_k0VQaG'); // Remplacez par votre clé publique EmailJS
+})();
+
+/*
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const nom = document.getElementById('nom').value;
+    const message = document.getElementById('message').value;
+    const mailto = `mailto:leonnitnomenjanahary@gmail.com?subject=Message de ${encodeURIComponent(nom)}&body=${encodeURIComponent(message + "\n\nEmail: " + email)}`;
+    window.location.href = mailto;
+});
+*/
